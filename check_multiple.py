c94fa7cb71abfa1cd35929cb435da0d6c93ec65a
Shraddha Atreya <111982092+ShraddhaAtreya@users.noreply.github.com>
2025-07-03 20:59:44 +0530

    Update check_multiple.py

--- check_multiple.py
+++ check_multiple.py
@@ -7,8 +7,8 @@ divisor = 3  # Change this to 4 to simulate a failed case
 print(f"Checking if {number} is a multiple of {divisor}...")
 
 if number % divisor == 0:
-    print(f"{number} is a multiple of {divisor}. ✅ Test Passed.")
+    print(f"{number} is a multiple of {divisor}. Test Passed.")
     exit(0)  # Jenkins marks this build as SUCCESS
 else:
-    print(f"{number} is NOT a multiple of {divisor}. ❌ Test Failed.")
+    print(f"{number} is NOT a multiple of {divisor}.  Test Failed.")
     exit(1)  # Jenkins marks this build as FAILURE
